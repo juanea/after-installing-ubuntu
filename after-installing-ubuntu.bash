a8aed82e43db3a105c1104a2c4696bddaec505f6
Juan Echavarria <juanea@gmail.com>
2019-11-02 16:44:11 -0700

    Create after-installing-ubuntu.bash
    Set of programs and commands after installing Ubuntu.

---
+++ after-installing-ubuntu.bash
@@ -0,0 +1,27 @@
+#!/bin/bashsudo apt -y update
+
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y full-upgrade
+sudo apt -y auto-remove
+
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt -y update
+sudo apt install -y git
+sudo apt install -y vim
+sudo apt install -y texlive-full
+sudo apt install -y kazam
+sudo apt install -y tmux
+
+sudo snap install skype --classic
+sudo snap install code --classic
+sudo snap install gimp
+sudo snap install inkscape
+sudo snap install vlc
+
+# Multiple workspaces for ubuntu desktop
+gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 3
+gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 3
+
+#TODO: copy bash_aliases into the proper directory
+#update bash_aliases
