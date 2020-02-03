3679340f12c88c9fa3234b40b50e6c6b75d96354
Juan Echavarria <j.echvarria@ai-systems.ca>
2020-02-03 14:06:55 -0800

    Update for ubuntu 18 and install docker

--- after-installing-ubuntu.bash
+++ after-installing-ubuntu.bash
@@ -57,3 +57,6 @@ sudo usermod -aG docker $USER
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+git config --global user.email "j.echvarria@ai-systems.ca"
+git config --global user.name "Juan Echavarria"
